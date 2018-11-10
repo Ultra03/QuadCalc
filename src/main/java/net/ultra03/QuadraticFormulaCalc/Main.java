05a01c98579be852f760dc69d272178a6570712e
= <ultramlg1108@gmail.com>
2018-11-09 21:50:18 -0800

    Add main files

---
+++ src/main/java/net/ultra03/QuadraticFormulaCalc/Main.java
@@ -0,0 +1,8 @@
+package net.ultra03.QuadraticFormulaCalc;
+
+public class Main {
+	public static void main(String[] args) {
+		QuadCalc calc = new QuadCalc();
+		calc.calculate();
+	}
+}
---
+++ src/main/java/net/ultra03/QuadraticFormulaCalc/QuadCalc.java
@@ -0,0 +1,35 @@
+package net.ultra03.QuadraticFormulaCalc;
+
+import java.util.Scanner;
+
+public class QuadCalc {
+	
+	public void calculate() {
+		// Initialize Scanner for user input
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Value for a: ");
+		double a = scan.nextDouble();
+		System.out.println("Value for b: ");
+		double b = scan.nextDouble();
+		System.out.println("Value for c: ");
+		double c = scan.nextDouble();
+		scan.close();
+		
+		double firstAnswer = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+		double secondAnswer = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		if(Double.isNaN(firstAnswer) || Double.isNaN(secondAnswer)) {
+			System.out.println("Values entered are not applicable to the quadratic formula (they're imaginary numbers)");
+		} else {
+			System.out.println("The answers are:");
+			System.out.println(firstAnswer);
+			System.out.println(secondAnswer);
+		}
+		
+	}
+
+}
